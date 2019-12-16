df383b4cd99054e233f05fb1f0b185251e0f29b2
Moses Ogwo <mosesogwo@gmail.com>
2019-12-16 23:36:52 +0100

    add submit event listener function to run getWeatherC

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ const cityCloud = responseArea.querySelector('.cloud-content')
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
   const cityValue = document.querySelector('#city').value;
-  getWeatherC(cityValue);
-  responseArea.classList.remove("hidden");
-  document.querySelector('#city').value = "";
+  if (cityValue != '') {
+    getWeatherC(cityValue);
+    responseArea.classList.remove("hidden");
+    document.querySelector('#city').value = "";
+  }
 })
 
 const getWeatherC = (city) => {
@@ -26,19 +28,23 @@ const getWeatherC = (city) => {
     console.log(response)
     cityName.innerHTML = `${response.name}, ${response.sys.country} <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt="weather-icon">`;
     cityDesc.textContent = `${response.weather[0].description}`;
-    cityTemp.textContent = `${response.main.temp}C`;
+    cityTemp.textContent = `${response.main.temp} C`;
     cityWind.textContent = `${response.wind.speed}`;
     cityCloud.textContent = `${response.clouds.all}`;
   })
 }
 
-// const getWeatherF = (city) => {
-//   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${appID}&units=imperial`)
-//   .then((response) => {
-//     return response.json()
-//   })
-//   .then((response) => {
-//     const weatherData = response;
-//     console.log(weatherData);
-//   })
-// }
+const getWeatherF = (city) => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${appID}&units=imperial`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((response) => {
+    console.log(response)
+    cityName.innerHTML = `${response.name}, ${response.sys.country} <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt="weather-icon">`;
+    cityDesc.textContent = `${response.weather[0].description}`;
+    cityTemp.textContent = `${response.main.temp} F`;
+    cityWind.textContent = `${response.wind.speed}`;
+    cityCloud.textContent = `${response.clouds.all}`;
+  })
+}
